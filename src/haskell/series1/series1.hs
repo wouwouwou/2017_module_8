14c10b3bfb3402bad9d4840746fdb736dbce08cb
Wouter Bos <wouter_bos96@hotmail.com>
2017-04-26 12:26:42 +0200

    Great merge of fp series 1! :D

--- src/haskell/series1/series1.hs
+++ src/haskell/series1/series1.hs
@@ -5,6 +5,8 @@ import Data.Char
 
 ex1 x = 2*x^2 + 3*x - 5
 
+
+-- Exercise 2
 abc = "abcdefghijklmnopqrstuvwxyz"
 
 code :: Char -> Char
@@ -32,6 +34,8 @@ codestr = map code "hello"
 codestr2 :: [Char]
 codestr2 = map code "Tomorrow evening, 8 o\'clock in Amsterdam"
 
+
+--Exercise 3
 interest :: Floating a => a -> a -> a -> a
 interest a r n        = (a*(1+r)**n)
 
@@ -39,17 +43,25 @@ amount :: (Floating a, Eq a) => a -> a -> a -> a
 amount a r 0    = a
 amount a r n    = amount (a*(1+r)) r (n-1)
 
+
+-- Exercise 4
+-- Nice test function, not part of the exercise. Can be extended to catch the errors of complex roots.
+roots :: Double -> Double -> Double -> (Double, Double)
+roots a b c = (root1 a b c, root2 a b c)
+
 root1 ::  Double -> Double -> Double -> Double
 root1 a b c     | disc a b c < 0        = error "negative discriminant"
-                | otherwise                = (-b + sqrt(disc a b c)) / (2*a)
+                | otherwise             = (-b + sqrt(disc a b c)) / (2*a)
 
 root2 ::  Double -> Double -> Double -> Double
 root2 a b c     | disc a b c < 0        = error "negative discriminant"
-                | otherwise                = (-b - sqrt(disc a b c)) / (2*a)
+                | otherwise             = (-b - sqrt(disc a b c)) / (2*a)
 
 disc ::  Double -> Double -> Double -> Double
 disc a b c      = (b*b)-4*a*c
 
+
+-- Exercise 5
 extrX :: Double -> Double -> Double -> Double
 extrX a b c = -b/(2*a)
 
@@ -58,6 +70,7 @@ extrY a b c = a*(exX)^2 + b*(exX) + c
     where exX = extrX a b c
 
 
+-- Exercise 6
 mylength :: [a] -> Int
 mylength []     = 0
 mylength (x:xs) = 1 + mylength xs
@@ -91,31 +104,32 @@ mymaximum (x:xs)        | x <= mymaximum(xs)    = mymaximum(xs)
                         | otherwise             = x
 
 myzip :: [a] -> [b] -> [(a, b)]
-
 myzip (x:xs) []         = []
 myzip [] (y:ys)         = []
 myzip (x:xs) (y:ys)     = (x,y) : myzip xs ys
 
 
+-- Exercise 7
 r :: Num a => a -> a -> [a]
 r a d   = a : r (a+d) d
 
---r1 :: Num a Eq a => a -> a -> a -> a
+r1 :: Num a => a -> a -> Int -> a
 r1 a d 0        = a
-r1 a d n        = r1 (a+d) d (n-1) -- l!!n
+r1 a d n        = (r a d)!!n
 
---total
---total :: Int a => a -> a -> a -> a -> a
-
+--total1 is better
+total :: (Eq a, Num a) => Int -> Int -> a -> Int -> Int
 total a d i 0   = 0
 total a d 0 j   = mysum (mytake j (r a d))
 total a d i j   = total (a+d) d (i-1) (j-1)
 
+total1 :: (Num a) => a -> a -> Int -> Int -> a
 total1 a d i j  | i == j        = (r1 a d j)
                 | i > j         = error "i must be smaller than j"
                 | otherwise     = (r1 a d i) + total1 a d (i+1) j
 
 
+-- Exercise 8
 allEqual :: Eq a => [a] -> Bool
 allEqual []     = True
 allEqual [x]    = True
@@ -131,15 +145,21 @@ isAS' [_,_] = True
 isAS' xss@(x1:x2:x3:xs) | x2-x1 == x3-x2    = isAS' (x2:x3:xs)
                         | otherwise         = False
 
-matrixSquare :: [[a]] -> Bool
-matrixSquare a = allEqual(map mylength a)
+
+-- Exercise 9
+isMatrix :: [[a]] -> Bool
+isMatrix a = allEqual(map mylength a)
 
 rowTotals :: [[Int]] -> [Int]
 rowTotals a = map mysum a
 
-transposeMatrix :: [[a]] -> [[a]]
-transposeMatrix a       | mylength (head a) == 0        = []
-                        | otherwise                     = map head a : transposeMatrix (map tail a)
+transposeMatrix :: (Eq a) => [[a]] -> [[a]]
+transposeMatrix a       | head a == []   = []
+                        | otherwise      = map head a : transposeMatrix (map tail a)
 
+columnTotals :: [[Int]] -> [Int]
 columnTotals a          | mylength (head a) == 0        = []
                         | otherwise                     = mysum(map head a) : columnTotals (map tail a)
+
+columnTotals' :: [[Int]] -> [Int]
+columnTotals' a = rowTotals $ transposeMatrix a
