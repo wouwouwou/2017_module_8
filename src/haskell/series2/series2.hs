6b636f1ce9cfa47d12fc2861936acde51619abea
Martijn Verkleij <github@mverkleij.nl>
2017-05-01 12:21:20 +0200

    tabs -> spaces in series2.hs

--- src/haskell/series2/series2.hs
+++ src/haskell/series2/series2.hs
@@ -3,113 +3,113 @@ import Data.List
 
 -------------------------------------------------
 allEqual :: Eq a => [a] -> Bool
-allEqual [] 	= True
-allEqual [x]	= True
-allEqual (x:xs)	| x == head xs	= allEqual xs
-		| otherwise	= False
+allEqual []     = True
+allEqual [x]    = True
+allEqual (x:xs)    | x == head xs    = allEqual xs
+        | otherwise    = False
 -------------------------------------------------
 
 
 -- Exercise 1
 myfilter :: (a -> Bool) -> [a] -> [a]
-myfilter _ [] 		= []
-myfilter f (x:xs)	| f x		= x : myfilter f xs
-					| otherwise	= myfilter f xs
+myfilter _ []         = []
+myfilter f (x:xs)    | f x        = x : myfilter f xs
+                    | otherwise    = myfilter f xs
 
 myfoldl :: (a -> b -> a) -> a -> [b] -> a
-myfoldl f a [x]		= a `f` x
-myfoldl f a (x:xs)	= myfoldl f (a `f` x) xs
+myfoldl f a [x]        = a `f` x
+myfoldl f a (x:xs)    = myfoldl f (a `f` x) xs
 
 myfoldr :: (a -> b -> b) -> b -> [a] -> b
-myfoldr f a [x]		= x `f` a
-myfoldr f a xs 		= myfoldr f ((last xs) `f` a) (init xs)
+myfoldr f a [x]        = x `f` a
+myfoldr f a xs         = myfoldr f ((last xs) `f` a) (init xs)
 
 
 myzipWith :: (a -> b -> c) -> [a] -> [b] -> [c]
 
-myzipWith _ [] _		= []
-myzipWith _ _ []		= []
-myzipWith f (x:xs) (y:ys)	= x `f` y : myzipWith f xs ys
+myzipWith _ [] _        = []
+myzipWith _ _ []        = []
+myzipWith f (x:xs) (y:ys)    = x `f` y : myzipWith f xs ys
 
 
 --------------------------
---	   Excercise 2		--
+--       Excercise 2        --
 --------------------------
 gba :: [(String,Int,String,String)]
 gba = [("a", 15, "M", "Soest"), ("b", 37, "F", "Soest"), ("c", 12, "M", "Soest"), ("d", 25, "F", "Amersfoort")]
 
-getName (a,b,c,d)	= a
-getAge (a,b,c,d)	= b
-getSex (a,b,c,d)	= c
-getCity (a,b,c,d)	= d
+getName (a,b,c,d)    = a
+getAge (a,b,c,d)    = b
+getSex (a,b,c,d)    = c
+getCity (a,b,c,d)    = d
 
 --B
 getByName :: String -> [(String, Int, String, String)] -> (String, Int, String, String)
-getByName _ []	= error "not found"
-getByName q (x:xs)	| map (toLower) (getName x) == map (toLower) (q) = x
-					| otherwise                                      = getByName q xs
+getByName _ []    = error "not found"
+getByName q (x:xs)    | map (toLower) (getName x) == map (toLower) (q) = x
+                    | otherwise                                      = getByName q xs
 
-getNameByName q xs	= getName (getByName q xs)
-getAgeByName q xs	= getAge (getByName q xs)
-getSexByName q xs	= getSex (getByName q xs)
-getCityByName q xs	= getCity (getByName q xs)
+getNameByName q xs    = getName (getByName q xs)
+getAgeByName q xs    = getAge (getByName q xs)
+getSexByName q xs    = getSex (getByName q xs)
+getCityByName q xs    = getCity (getByName q xs)
 
 
 --C
 --ageRec :: b -> [(a,b,c,d)] -> [(a,b,c,d)]
-ageRec a [] 	= []
+ageRec a []     = []
 ageRec a (x:xs) = (getName x,(getAge x) + a,getSex x ,getCity x) : ageRec a xs
 
-ageList a xs	= [ (getName x,getAge x + a,getSex x ,getCity x) | x <- xs ]
+ageList a xs    = [ (getName x,getAge x + a,getSex x ,getCity x) | x <- xs ]
 
 incAge n (a,b,c,d) = (a,b+n,c,d)
 
-ageHOF a xs	= map (incAge a) xs
+ageHOF a xs    = map (incAge a) xs
 
 --D
-is30to40F (a,b,c,d) 	| b >= 30 && b <= 40 && c == "F"	= True
-			| otherwise				= False
+is30to40F (a,b,c,d)     | b >= 30 && b <= 40 && c == "F"    = True
+            | otherwise                = False
 
-get30to40Rec []		= []
-get30to40Rec (x:xs)	| is30to40F x 	= x : get30to40Rec xs
-			| otherwise							= get30to40Rec xs
+get30to40Rec []        = []
+get30to40Rec (x:xs)    | is30to40F x     = x : get30to40Rec xs
+            | otherwise                            = get30to40Rec xs
 
-get30to40List xs	= [ (getName x, getAge x, getSex x, getCity x) | x <- xs, is30to40F x ]
+get30to40List xs    = [ (getName x, getAge x, getSex x, getCity x) | x <- xs, is30to40F x ]
 
 
-get30to40HOF xs		= filter (is30to40F) xs
+get30to40HOF xs        = filter (is30to40F) xs
 
 --E: see B
 
 --F
-swapAB (a,b,c,d)	= (b,a,c,d)
+swapAB (a,b,c,d)    = (b,a,c,d)
 
-sortByAge xs	= map (swapAB) (sort (map (swapAB) xs))
+sortByAge xs    = map (swapAB) (sort (map (swapAB) xs))
 
 ----------------------------------
---	   Excercise 3		--
+--       Excercise 3        --
 ----------------------------------
 --a
-sieve []	= []
-sieve (x:xs)	= x: sieve (filter (\y-> mod y x /= 0) (xs))
+sieve []    = []
+sieve (x:xs)    = x: sieve (filter (\y-> mod y x /= 0) (xs))
 
 isPrime :: Integer -> Bool
 
-isPrime x	= elem x (sieve [2..x])
+isPrime x    = elem x (sieve [2..x])
 
-firstNPrimes x	= take x (sieve [2..])
+firstNPrimes x    = take x (sieve [2..])
 
-primesTo x 	= sieve [2..x]
+primesTo x     = sieve [2..x]
 
 --b
 
 dividers :: Int -> [Int]
-dividers n	= [ x | x <- [2..n], mod n x == 0 ]
+dividers n    = [ x | x <- [2..n], mod n x == 0 ]
 
-altPrimes n 	= (length (dividers n)) == 1
+altPrimes n     = (length (dividers n)) == 1
 
 ----------------------------------
---	   Excercise 4		--
+--       Excercise 4        --
 ----------------------------------
 --a
 pyth :: (Integral t) => t -> [(t, t, t)]
@@ -121,35 +121,35 @@ pythUniq n = [(x,y,z) |x <- [1..n], y <- [1..n], z <- [1..n], x < y, isPrime z, 
 
 
 ----------------------------------
---	   Excercise 5		--
+--       Excercise 5        --
 ----------------------------------
 --a
 increasing :: (Ord a, Num a) => [a] -> Bool
-increasing [x]	= True
+increasing [x]    = True
 increasing (x1:x2:xs) = x1 < x2 && increasing (x2:xs)
 --b
 weaklyIncreasing :: (Ord a, Num a, Fractional a, Enum a) => [a] -> Bool
-weaklyIncreasing [x] 	= True
-weaklyIncreasing xs 	= increasing (zipWith (/) (scanl (+) 0 xs) [1..])
+weaklyIncreasing [x]     = True
+weaklyIncreasing xs     = increasing (zipWith (/) (scanl (+) 0 xs) [1..])
 
 ---ex6
 --a
 
 
-sublist (x:xs) yss@(y:ys)	| x == y		= allEqual (zipWith (==) xs ys)
-				| otherwise		= sublist (x:xs) ys
+sublist (x:xs) yss@(y:ys)    | x == y        = allEqual (zipWith (==) xs ys)
+                | otherwise        = sublist (x:xs) ys
 --TODO fails on [1,4,5,1,2,3] with input [1,2,3]
 
 --b
-psublist [] _		= True
-psublist _ []		= False
-psublist (x:xs) (y:ys)	| x == y	= psublist xs ys
-			| otherwise	= psublist (x:xs) ys
+psublist [] _        = True
+psublist _ []        = False
+psublist (x:xs) (y:ys)    | x == y    = psublist xs ys
+            | otherwise    = psublist (x:xs) ys
 
 
 ---ex7
 --a
---bsort xs	=
+--bsort xs    =
 --ins :: Ord a => a -> [a] -> [a]
 
 bsort :: Ord a => [a] -> [a]
