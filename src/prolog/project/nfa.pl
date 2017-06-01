037a739c9c787acee976e2ec6ebb54814966c8b4
Wouter Bos <wouter_bos96@hotmail.com>
2017-06-01 11:34:18 +0200

    LP - Exercise 1 project done

--- src/prolog/project/nfa.pl
+++ src/prolog/project/nfa.pl
@@ -16,21 +16,15 @@ example(n2, nfa(0,
   trans(0,b,0),trans(0,b,3),trans(3,b,4),trans(4,b,5),trans(5,b,5)],
  [5])).
 
- % example n3 equals n1, but also accepts state 1.
- %
- example(n3, nfa(0,[trans(0,a,0), trans(0,a,1),trans(1,b,1),trans(1,b,2)], [1, 2]) ).
+% example n3 equals n1, but also accepts state 1.
+%
+example(n3, nfa(0,[trans(0,a,0), trans(0,a,1),trans(1,b,1),trans(1,b,2)], [1, 2]) ).
 
- % example n4 equals n3, but is not a valid nfa.
- %
- example(n4, nfa(b,[trans(0,a,0), trans(0,a,1),trans(1,b,1),trans(1,b,2)], [1, 2]) ).
+% example n4 equals n3, but is not a valid nfa.
+%
+example(n4, nfa(b,[trans(0,a,0), trans(0,a,1),trans(1,b,1),trans(1,b,2)], [1, 2]) ).
 
-% Question 1: alphabet/2
-
-%alphabet(nfa(_, T, _), L) :- L = Z, labels(T, Z).
-
-%labels([],_).
-%labels([T|Ts], Z) := labels(Ts, [label(T)|Z]).
-
+% Helper functions. We like them and they are nice to use.
 % Try this: example(n3, N), isnfa(N), init(N, I),
 %           transitions(N, Ts), accepting(N, A).
 
@@ -38,6 +32,12 @@ init(nfa(Init, _, _), I) :- I = Init.
 accepting(nfa(_, _, Accepting), A) :- A = Accepting.
 transitions(nfa(_, Transitions, _), Ts) :- Ts = Transitions.
 
+labels([], []) :- !.
+labels([trans(_, Label, _)|Ts], Ls) :- labels(Ts, T), list_to_set([Label|T], Ls).
+
+from(trans(From, _, _), F) :- F = From.
+label(trans(_, Label, _), L) :- L = Label.
+to(trans(_, _, To), T) :- T = To.
 
 isnfa(nil).
 isnfa(nfa(Init, Transitions, Accepting)) :-
@@ -54,6 +54,10 @@ isTransistion(trans(S1, L, S2)) :- isState(S1), is_alpha(L), isState(S2).
 
 areStates([]).
 areStates([S|States]) :- isState(S), areStates(States).
+
+% Question 1: alphabet/2
+
+alphabet(N, L) :- transitions(N, Ts), labels(Ts, L).
 
 
 % Question 2: testNFA/2
